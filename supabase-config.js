1b119fd3e5f3e155c3a7ea45aa890eaf6df9d89b
Hugo22M <hugocanaveral22@gmail.com>
2026-05-28 12:44:08 +0200

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = "https://msucywjvyktipexeogw.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_3MpjEyZXIQ7t5_MvjedzqQ_GoOfv7UV";
-
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
