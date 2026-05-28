11009e5cdc1adce741239989c317464c278d29b2
Hugo22M <hugocanaveral22@gmail.com>
2026-05-28 12:44:24 +0200

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://msucyvjwyktipexreogw.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_3MpjEyZXIQ7t5_MvjedzqQ_GoOfv7UV";
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
