dd41b4f991e17789a2825d82499afb4d495904d3
Hugo22M <hugocanaveral22@gmail.com>
2026-05-28 23:24:10 +0200

    Update registro.js

--- registro.js
+++ registro.js
@@ -1,19 +1,22 @@
 var formRegistro = document.getElementById("formRegistro");
 
-formRegistro.addEventListener("submit", function(evento) {
-    evento.preventDefault();
+if (formRegistro) {
+    formRegistro.addEventListener("submit", function(evento) {
+        evento.preventDefault();
 
-    var usuario = document.getElementById("nuevoUsuario").value.trim();
-    var password = document.getElementById("nuevaPassword").value.trim();
+        var usuario = document.getElementById("usuarioRegistro").value.trim();
+        var password = document.getElementById("passwordRegistro").value.trim();
 
-    if (usuario === "" || password === "") {
-        alert("Rellena todos los campos");
-        return;
-    }
+        if (usuario === "" || password === "") {
+            alert("Rellena todos los campos");
+            return;
+        }
 
-    localStorage.setItem("usuarioRegistrado", usuario);
-    localStorage.setItem("passwordRegistrada", password);
+        localStorage.setItem("usuarioRegistrado", usuario);
+        localStorage.setItem("passwordRegistrada", password);
 
-    alert("Cuenta creada correctamente");
-    window.location.href = "login.html";
-});
+        alert("Cuenta creada correctamente");
+
+        window.location.href = "login.html";
+    });
+}
